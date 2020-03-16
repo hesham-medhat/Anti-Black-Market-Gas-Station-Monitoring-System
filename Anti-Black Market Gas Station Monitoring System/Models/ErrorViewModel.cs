615e9eddf8bbcb9d944a1f62f85d9ebf9f7310c0
hesham-medhat <heshammedhat5@gmail.com>
2020-03-17 00:16:27 +0200

    Implement database schema and initialize solution and web app with authentication

---
+++ Anti-Black Market Gas Station Monitoring System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Anti_Black_Market_Gas_Station_Monitoring_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GSMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GSMS.Startup))]
+namespace GSMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
